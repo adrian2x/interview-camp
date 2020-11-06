3a43306546b17ceaf9379cb77b5ab81bf0b19167
Adrian <adr.ca@live.com>
2020-11-06 13:17:25 -0800

    BST tests

--- data_structures/BST.py
+++ data_structures/BST.py
@@ -1,3 +1,12 @@
+""" Binary Search Tree (BST)
+
+    A BST is a collection of Nodes composed of key-value pairs and in which each node key is inserted following this rule:
+        Node.left <= Node < Node.right
+
+    The keys must be comparable, while the value can be any arbitrary data.
+    A notable property of the BST is that it is iterable, producing the keys in ascending order, and also supports reverse iteration for descending order.
+"""
+from typing import Iterator
 from collections.abc import Collection
 
 
@@ -66,21 +75,34 @@ class Node:
 
     def dispose(self):
         "Clean up references to other nodes"
-        self.left = None
-        self.right = None
-        self.parent = None
+        del self.value
+        del self.left
+        del self.right
+        del self.parent
+
+    def __min__(self):
+        while self.left:
+            self = self.left
+        return self
+
+    def __max__(self):
+        while self.right:
+            self = self.right
+        return self
 
 
-class BST:
+class BST(Collection):
     "Binary Search Tree"
 
     def __init__(self):
         self.root = None
+        self.size = 0
 
     def add(self, k, v=None):
         "Add key to the tree"
 
         # Check for root node
+        self.size += 1
         if self.root is None:
             self.root = Node(k, v)
         else:
@@ -93,14 +115,21 @@ class BST:
         node = self.root.search(k)
         return node is not None
 
+    def __len__(self):
+        return self.size
+
     def remove(self, k):
         "Remove a key from the tree"
         if self.root is None:
             raise ValueError("value not in tree")
 
         node = self.root.search(k)  # find node to delete
+        if node is None:
+            raise ValueError("value not in tree")
 
+        self.size -= 1
         if node is self.root:
+            # Remove the root and replace
             if node.left is None:
                 node = node.right
             elif node.right is None:
@@ -115,3 +144,79 @@ class BST:
             self.root = node
         else:
             node.remove()
+
+    def preorder(self):
+        return preorder(self.root)
+
+    def inorder(self):
+        return inorder(self.root)
+
+    def postorder(self):
+        return postorder(self.root)
+
+    def __iter__(self):
+        "Default to in-order traversal"
+        return inorder(self.root)
+
+    def __reversed__(self) -> Iterator:
+        return reverse_inorder(self.root)
+
+    def max(self, k=0):
+        "Return the max or k-th max value in the tree"
+        if self.root is None:
+            return None
+        if not k:
+            return max(self)
+
+        if k > self.size:
+            # check if k is out of bounds
+            raise IndexError('index out of range')
+        else:
+            # create a reverse in-order iterator
+            _iter = reverse_inorder(self.root)
+            # take the first k values
+            while k > 0:
+                value = next(_iter)
+                k -= 1
+            return value
+
+    def __min__(self):
+        if self.root:
+            return min(self.root).key
+
+    def __max__(self):
+        if self.root:
+            return max(self.root).key
+
+
+def preorder(root):
+    if root is None:
+        return ()
+    yield root.key
+    yield from preorder(root.left)
+    yield from preorder(root.right)
+
+
+def inorder(root):
+    if root is None:
+        return ()
+    yield from inorder(root.left)
+    yield root.key
+    yield from inorder(root.right)
+
+
+def reverse_inorder(root):
+    "Traverse the tree in a reverse in-order sequence, which results in descending order"
+    if root is None:
+        return ()
+    yield from reverse_inorder(root.right)
+    yield root.key
+    yield from reverse_inorder(root.left)
+
+def postorder(root):
+    if root is None:
+        return ()
+    yield from postorder(root.left)
+    yield from postorder(root.right)
+    yield root.key
+
--- data_structures/BST.test.py
+++ data_structures/BST.test.py
@@ -1,4 +1,4 @@
-from BST import BST
+from BST import BST, Node
 
 
 tree = BST()
@@ -8,10 +8,31 @@ tree.add(3)
 tree.add(6)
 tree.add(4)
 tree.add(7)
+
+print(">>> in-order:")
+for i in tree.inorder():
+    print(i)
+
+print(">>> post-order:")
+for i in tree.postorder():
+    print(i)
+
+print("min()", min(tree))
+print("max()", max(tree))
+assert max(tree) == 7
+
+# k-th max value (order statistics)
+assert tree.max(2) == 6
+assert tree.max(3) == 4
+
 assert 3 in tree
 tree.remove(3)
 assert 3 not in tree
 
+
+#############################
+##  Delete node test
+#############################
 tree = BST()
 tree.add(6)
 tree.add(4)
@@ -26,6 +47,10 @@ assert 9 in tree
 tree.remove(9)
 assert 9 not in tree
 
+
+#################################
+##  Root node tests
+#################################
 tree.remove(6)
 assert 6 not in tree
 assert tree.root.key == 8
@@ -38,3 +63,27 @@ tree.remove(1)
 assert tree.root.key == 0
 tree.remove(0)
 assert tree.root is None
+
+
+#################################
+##  Validate BST
+#################################
+n1 = Node(1)
+root = Node(2)
+root.left = n1
+
+def is_bst(root):
+    if root is None:
+        return True
+    if root.left and root.left.key > root.key:
+        return False
+    if root.right and root.right.key < root.key:
+        return False
+    return is_bst(root.left) and is_bst(root.right)
+
+assert is_bst(root) == True
+
+
+fake_tree = Node(0, right=n1)
+n1.left = root
+assert is_bst(fake_tree) == False
