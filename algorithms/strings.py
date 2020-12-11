4e33a2a6accb2b7bcac7d772aad1684762b21528
Adrian Cruz <adr.ca@live.com>
2020-12-11 13:45:41 -0800

    update rolling hash impl

--- algorithms/strings.py
+++ algorithms/strings.py
@@ -1,103 +1,49 @@
-
-
-def find_permutation(str1, pattern):
-    freq = {}
-    patlen = len(pattern)
-    for c in pattern:
-        if not c in freq:
-            freq[c] = 0
-        freq[c] += 1
-
-    start = 0
-    end = 0
-    matched = 0
-    while end < len(str1):
-        char = str1[end]
-        if char in freq:
-            freq[char] -= 1
-            if freq[char] == 0:
-                matched += 1
-
-        if matched == patlen:
-            return True
-
-        if end - start >= patlen:
-            # shrink the window from the start
-            leftchar = pattern[start]
-                if leftchar in 
-
-        end += 1
-
-    return False
-
-
-def is_perm(str1, other):
-  chars1 = {}
-  chars2 = {}
-  for c in str1:
-    if c not in chars1:
-      chars1[c] = 0
-    chars1[c] += 1
-
-  for c in other:
-    if c not in chars1:
-      return False
-    if c not in chars2:
-      chars2[c] = 0
-    chars2[c] += 1
-
-  for k in chars1:
-    if chars1[k] != chars2[k]:
-      return False
-
-  return True
-
-
-find_permutation("aaacb", "abc")
-
-
-def rabin_karp(text: str, search: str):
+def rabin_karp(text: str, search: str, base=127):
     """
     A Rabin-Karp implementation to find occurrences of search in text, in O(N + M)
     text: the text where we should search for pattern
     search: the pattern to search for in text
-    mod: a prime number
+    base: the alphabet size (eg. 127 for ascii)
     """
-    if text is None or search is None:
-        return -1
-    if text == "" or search == "":
-        return -1
-    if len(search) > len(text):
+    N, M = len(text), len(search)
+
+    if N < 1 or M < 1 or M > N:
         return -1
 
+    if N < 48: return text.find(search)
+    if text.endswith(search): return N - M
+
+    MOD = 1000000007 # prevent overflow (or upcasting)
 
     def rollhash(seq: str, length):
         "Create a hash by adding all characters in the string"
         value = 0
         for i in range(length):
-            value += ord(seq[i])
+            value = (value * base + ord(seq[i])) % MOD
         return value
 
     # hash the first M characters of text
-    N, M = len(text), len(search)
     hash_text = rollhash(text, M)
     hash_search = rollhash(search, M)
+    coeff = pow(base, M - 1, MOD) # first coefficient
 
     # check for all sequences of M characters in text
-    for i in range(N - M + 1):
+    for i in range(N - M):
         if hash_search == hash_text:
+            # check if we found the pattern
             for p in range(M):
-                print("submatch at", i + p)
                 if search[p] != text[i + p]:
                     break
             else:
                 return i
 
         # update rolling hash of the text window
-        hash_text -= ord(text[i])
-        hash_text += ord(text[i + M])
+        # by subtracting the first letter and adding the new one to the end
+        hash_text = (hash_text - ord(text[i]) * coeff) % MOD
+        hash_text = (hash_text * base + ord(text[i + M])) % MOD
 
     return -1
 
 
-rabin_karp("doe are hearing me", "ear")
+assert rabin_karp("doe are hearing me", "ear") == 9
+assert rabin_karp("doe are hearing me", " me") == 15
